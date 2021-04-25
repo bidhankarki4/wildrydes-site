e54147c297de17bc423a2a79e5dc811ce4d36128
Bidhan Karki <bidhan@MacBook-Pro.local>
2021-04-25 18:58:10 -0500

    update

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_UOnIGpg6c', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1qi46npukd01vm47kgr4em1hks', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_TXBk4WyyS', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4q31rodcsvtso0enrgdrn8333u', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
